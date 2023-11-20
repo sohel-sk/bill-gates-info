884d9f37fc0f09f86e5160278b12216e5c3299ba
sohel-sk <sohelsheikh9623@gmail.com>
2023-11-20 23:28:01 +0530

    vv

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello this is home to this page</h1>
+    <div>
+        <div><a href="/aboutus">About us</a></div>
+        <div><a href="/contact">Connect with us</a></div>
+        <div><a href="/history">history</a></div>
+        <div><a href="/details">Details</a></div>
+    </div>
+</body>
+</html>
